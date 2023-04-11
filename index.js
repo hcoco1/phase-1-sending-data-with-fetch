f4ff5422dc1fd55a85c3d1259f50e2c864840a8d
Ivan <arias.ivan@gmail.com>
2023-04-11 01:16:15 -0400

    lab done

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+const submitData = function(userName, userEmail){
+  return fetch('http://localhost:3000/users', {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+      },
+      body: JSON.stringify({
+          name: userName,
+          email: userEmail,
+      }),
+     
+  }).then(function(response){
+      return response.json();
+  })
+  .then(function(object){
+      const id = object.id;
+      const newElement = document.createElement('article');
+      newElement.textContent = id;
+      document.body.appendChild(newElement);
+
+  })
+  .catch(function(error){
+      const message = document.createElement('article')
+      message.textContent = error.message
+      document.body.appendChild(message);
+
+  })
+}
+submitData('ivan', 'arias.ivan@hotmail.com')
